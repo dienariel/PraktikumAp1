1c7e72877b615a223d519b3cd03fb1f8c62ee22c
dienariel <146154018+dienariel@users.noreply.github.com>
2023-09-27 10:29:36 +0700

    Add files via upload

---
+++ Tugas Pertemuan2/Tugas minggu ke 2 Ariel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int  TUGAS = 100;
+  int  UTS = 100;
+  int  UAS = 100;
+  float total = TUGAS+UTS+UAS;
+  float ratarata = 3;
+
+  cout << total/ratarata << endl;
+}
